9c51b35fdb34cc1230d14c6695cd32f04345cf9b
atinylittleshell <shell@atinylittleshell.me>
2026-03-14 23:00:00 -0700

    fix: shorter version display

--- cmd/root.go
+++ cmd/root.go
@@ -22,7 +22,7 @@ var rootCmd = &cobra.Command{
 	Long: `Treehouse maintains a pool of reusable, pre-warmed git worktrees
 so that multiple AI coding agents can work on the same repo in parallel.`,
 	Version:       version,
-	SilenceUsage:  true,
+	SilenceUsage: true,
 	SilenceErrors: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return getRunE(cmd, args)
@@ -50,6 +50,10 @@ so that multiple AI coding agents can work on the same repo in parallel.`,
 			_ = updater.SpawnBackgroundCheck(version)
 		}
 	},
+}
+
+func init() {
+	rootCmd.SetVersionTemplate(`{{.Version}}` + "\n")
 }
 
 func Execute() error {
